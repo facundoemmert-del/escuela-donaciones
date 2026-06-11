058a6dadf1ad85a8b005289e01fbc11304ef3751
facundoemmert-del <facundoemmert@gmail.com>
2026-06-10 21:24:30 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // ZIP 4 - Firebase Firestore
 // Reemplazá los valores de abajo por los datos de tu proyecto Firebase.
 // Firebase Console → Project settings → Your apps → Web app → Config
-
 export const firebaseConfig = {
-  apiKey: "PEGAR_API_KEY",
-  authDomain: "PEGAR_AUTH_DOMAIN",
-  projectId: "PEGAR_PROJECT_ID",
-  storageBucket: "PEGAR_STORAGE_BUCKET",
-  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
-  appId: "PEGAR_APP_ID"
+  apiKey: "AIzaSyAXAncRYENV-8U7werFykUB_aVetYBXu8",
+  authDomain: "escuela-donaciones.firebaseapp.com",
+  projectId: "escuela-donaciones",
+  storageBucket: "escuela-donaciones.firebasestorage.app",
+  messagingSenderId: "574524878875",
+  appId: "1:574524878875:web:73d28c2ed1115d1c7548be",
+  measurementId: "G-K07D1J946X"
 };
+
